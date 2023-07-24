5d397a8343bba7f2e6c48f18ea05aace3a833ad7
Enmendurana <97315736+Enmendurana@users.noreply.github.com>
2023-07-24 05:48:17 +0200

    Update dfs.cpp

--- Graph Algorithms/dfs.cpp
+++ Graph Algorithms/dfs.cpp
@@ -1,16 +1,16 @@
 /*
-  Naslov: Pregled v Globino
-  Problem: Iteracija skozi vsa vozlišča, do katerih lahko pridem iz vozlišča v
-  Čas: O(V + E)
-  Prostor: O(V + E)
+	Naslov: Pregled v Globino
+	Problem: Iteracija skozi vsa vozlišča, do katerih lahko pridem iz vozlišča v
+	Čas: O(V + E)
+	Prostor: O(V + E)
 */ 
 
 const int maxn = int(3e5) + 10;
 bool visited[maxn];
 
 void dfs(int v) {
-  visited[v] = true;
-  for(int u : G[v]) {
-    if(!visited[u]) dfs(u);
-  }
+	visited[v] = true;
+	for(int u : G[v]) {
+    	if(!visited[u]) dfs(u);
+	}
 }
