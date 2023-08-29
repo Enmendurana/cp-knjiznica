0d95e5d3f87e8214b9b307852051e944fba03cf5
Enmendurana <97315736+Enmendurana@users.noreply.github.com>
2023-08-29 04:18:21 +0200

    Create tarjan_scc.cpp

---
+++ Graph Algorithms/tarjan_scc.cpp
@@ -0,0 +1,31 @@
+vector<vector<int>> g;
+
+int timer = 0;
+int d[maxn], low[maxn];
+
+bool ok[maxn]; stack<int> st;
+int scc[maxn];
+
+void tarjan_scc(int v) {
+    d[v] = low[v] = ++timer;
+    ok[v] = true;
+    st.push(v);
+
+    for(int u : g[v]) {
+        if(d[u]==0) {
+            tarjan_scc(u);
+            low[v] = min(low[v],low[u]);
+        } else if(ok[u]) {
+            low[v] = min(low[v],d[u]);
+        }
+    }
+
+    if(d[v]==low[v]) {
+        int u = 0;
+        while(u != v) {
+            u = st.top(); st.pop();
+            ok[u] = false;
+            scc[u] = v;
+        }
+    }
+}
